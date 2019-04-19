6bc25e1f4fef9166519d44646e9a45cbbd512546
williamrmo <williamrmo84@gmail.com>
2019-04-18 19:52:38 -0600

    clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // users = ['Ryan', 'Cameron', 'Mario', 'Jon Snow', 'Arya Stark'];
-  // activated: boolean = false;
-  name: string = 'Fabian';
-  age: number;
-  adress: {
-    street: string;
-    city: string;
-  };
-  hobbies: string[];
-
-  constructor() {
-    this.age = 21;
-    this.adress = {
-      street: '221B Baker Street',
-      city: 'London'
-    };
-    this.hobbies = ['Swimming', 'Read', 'Write'];
-  }
 
 }
